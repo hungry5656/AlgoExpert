e2b3ef99fe06521b2de681cb493fa4de9e0661a8
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-14 01:18:02 -0700

    update code

--- Medium/SingleCycleCheck.cpp
+++ Medium/SingleCycleCheck.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 bool hasSingleCycle(vector<int> array) {
-    int index = 0, counter = 1, bound = array.size();
-    
-    return -1;
+    int index = 0, counter = 0, bound = array.size();
+    while (counter < bound){
+        index +=array[index];
+        if (index < 0){
+            while (index < 0){
+                index += bound;
+            }
+        } else if (index >= bound){
+            while (index >= bound){
+                index -= bound;
+            }
+        }
+        ++counter;
+        if ((counter < bound) && (index == 0)) return 0;
+    }
+    return index == 0;
 }
