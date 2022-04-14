fdbfefbabc5adf85719a8ff98f17d44c378c38e9
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-14 14:10:04 -0700

    finish youngestCommonAncestor.cpp

--- Medium/youngestCommonAncestor.cpp
+++ Medium/youngestCommonAncestor.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <map>
 using namespace std;
 
 class AncestralTree {
@@ -17,6 +18,35 @@ public:
 AncestralTree *getYoungestCommonAncestor(AncestralTree *topAncestor,
                                         AncestralTree *descendantOne,
                                         AncestralTree *descendantTwo) {
-    
-    return nullptr;
+    if (descendantOne == descendantTwo) return descendantOne -> ancestor;
+    int depthOne = 0, depthTwo = 0;
+    AncestralTree *currentNode = descendantOne;
+    while (currentNode != topAncestor){
+        currentNode = currentNode -> ancestor;
+        ++depthOne;
+    }
+    currentNode = descendantTwo;
+    while (currentNode != topAncestor){
+        currentNode = currentNode -> ancestor;
+        ++depthTwo;
+    }
+    int gaps = depthOne - depthTwo;
+    if (gaps > 0){
+        while(gaps != 0){
+            descendantOne = descendantOne -> ancestor;
+            --gaps;
+        }
+    } else if (gaps < 0){
+        while (gaps != 0){
+            descendantTwo = descendantTwo -> ancestor;
+            ++gaps;
+        }
+    }
+    while (descendantOne != topAncestor){
+        if (descendantOne == descendantTwo) return descendantOne;
+        descendantOne = descendantOne -> ancestor;
+        descendantTwo = descendantTwo -> ancestor;
+    }
+
+    return topAncestor;
 }
