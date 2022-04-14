7150fda9b2100a5bb91c41f6093efd39de19c161
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-14 14:30:08 -0700

    first versio

--- Easy/classPhotos.cpp
+++ Easy/classPhotos.cpp
@@ -1,7 +1,24 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 bool classPhotos(vector<int> redShirtHeights, vector<int> blueShirtHeights) {
-    
-    return false;
+    sort(redShirtHeights.begin(), redShirtHeights.end());
+    sort (blueShirtHeights.begin(), blueShirtHeights.end());
+    int compareNum = redShirtHeights[0] - blueShirtHeights[0];
+    if (compareNum == 0) return false;
+    if (compareNum > 0){
+        for (int i = 0; i < redShirtHeights.size(); ++i){
+            if (redShirtHeights[i] <= blueShirtHeights[i]){
+                return false;
+            }
+        }
+    } else {
+        for (int i = 0; i < redShirtHeights.size(); ++i){
+            if (redShirtHeights[i] >= blueShirtHeights[i]){
+                return false;
+            }
+        }
+    }
+    return true;
 }
