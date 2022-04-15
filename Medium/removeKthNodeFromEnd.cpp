6fd104781a21c5ad2f576fa8af3f4a818503538f
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-15 02:13:20 -0700

    update remove kth

--- Medium/removeKthNodeFromEnd.cpp
+++ Medium/removeKthNodeFromEnd.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 class LinkedList {
@@ -12,5 +13,20 @@ public:
 };
 
 void removeKthNodeFromEnd(LinkedList *head, int k) {
-    
+    LinkedList *currK = head, *currNode = head;
+    int gaps = 0;
+    while (currNode -> next != nullptr){
+        currNode = currNode -> next;
+        if (gaps >= k){
+            currK = currK ->next;
+        }
+        ++gaps;
+    }
+    if (gaps < k) {
+        head -> value = head -> next -> value;
+        head -> next = head -> next -> next;
+        return;
+    }
+    currK -> next = currK -> next -> next;
+
 }
