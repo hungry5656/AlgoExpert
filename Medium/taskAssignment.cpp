cb7d18f0e19e337ca7985e1e2587c0f578662bdf
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-15 00:17:18 -0700

    update taskassignment

--- Medium/taskAssignment.cpp
+++ Medium/taskAssignment.cpp
@@ -1,7 +1,17 @@
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 vector<vector<int>> taskAssignment(int k, vector<int> tasks) {
-    
-    return {};
+    sort(tasks.begin(), tasks.end());
+    int tsize = tasks.size() - 1;
+    vector<vector<int>> retVal;
+    for (int i = 0; i < k; ++i){
+        vector<int> currentVec;
+        currentVec.push_back(tasks[i]);
+        currentVec.push_back(tasks[tsize - i]);
+        retVal.push_back(currentVec);
+    }
+    return retVal;
 }
