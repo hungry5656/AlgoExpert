b291a1cf7f74bce57550876bc571c2a90a1f6842
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-15 00:54:37 -0700

    finish valid starting city

--- Medium/validStartingCity.cpp
+++ Medium/validStartingCity.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int validStartingCity(vector<int> distances, vector<int> fuel, int mpg) {
-    
-    return -1;
+    int retVal = 0, fuelbfArrive = 0, minfuel = 0;
+    for (int i = 0; i < distances.size(); ++i){
+        fuelbfArrive += (fuel[i] * mpg);
+        fuelbfArrive -= distances[i];
+        if (fuelbfArrive < minfuel){
+            retVal = i + 1;
+            minfuel = fuelbfArrive;
+        }
+    }
+    if (retVal == distances.size()){
+        retVal = 0;
+    }
+    return retVal;
 }
