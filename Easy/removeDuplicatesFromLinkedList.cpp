760384a156a9cb52dcb36fad2e9332f6b0b1345b
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-15 00:58:23 -0700

    init ll1

---
+++ Easy/removeDuplicatesFromLinkedList.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+// This is an input struct. Do not edit.
+class LinkedList {
+public:
+    int value;
+    LinkedList *next = nullptr;
+
+    LinkedList(int value) { this->value = value; }
+};
+
+LinkedList *removeDuplicatesFromLinkedList(LinkedList *linkedList) {
+    
+    return nullptr;
+}
