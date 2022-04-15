08d7f3bac49629f4803f567e0672e921a8066bc7
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-14 21:25:17 -0700

    finish tandem Bicycle

--- Easy/tandemBicycle.cpp
+++ Easy/tandemBicycle.cpp
@@ -1,8 +1,28 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int tandemBicycle(vector<int> redShirtSpeeds, vector<int> blueShirtSpeeds,
                 bool fastest) {
-    
-    return -1;
+    if (redShirtSpeeds.size() == 0) return 0;
+    sort(redShirtSpeeds.begin(), redShirtSpeeds.end());
+    sort(blueShirtSpeeds.begin(), blueShirtSpeeds.end());
+    int sum = 0;
+    if (fastest){
+        int i = redShirtSpeeds.size() -1, j = i;
+        while (i + j != redShirtSpeeds.size() - 2){
+            if (redShirtSpeeds[i] > blueShirtSpeeds[j]){
+                sum += redShirtSpeeds[i];
+                --i;
+            } else {
+                sum += blueShirtSpeeds[j];
+                --j;
+            }
+        }
+    } else {
+        for (int i = 0; i < redShirtSpeeds.size(); ++i){
+            sum += (redShirtSpeeds[i] > blueShirtSpeeds[i]) ? redShirtSpeeds[i] : blueShirtSpeeds[i];
+        }
+    }
+    return sum;
 }
