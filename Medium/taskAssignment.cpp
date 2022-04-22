0e40ab907063caea7818160b0cb0ac800fdb0ea7
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-22 00:15:38 -0700

    finish task assignment

--- Medium/taskAssignment.cpp
+++ Medium/taskAssignment.cpp
@@ -4,13 +4,24 @@
 using namespace std;
 
 vector<vector<int>> taskAssignment(int k, vector<int> tasks) {
-    sort(tasks.begin(), tasks.end());
+    unordered_map<int, vector<int>> uMap;
+    for (int i = 0; i < 2 * k; ++i){
+        if (uMap.find(tasks[i]) == uMap.end()){
+            vector<int> cV = {i};
+            uMap.emplace(tasks[i], cV);
+        } else {
+            uMap[tasks[i]].push_back(i);
+        }
+    }
+    std::sort(tasks.begin(), tasks.end());
     int tsize = tasks.size() - 1;
     vector<vector<int>> retVal;
     for (int i = 0; i < k; ++i){
         vector<int> currentVec;
-        currentVec.push_back(tasks[i]);
-        currentVec.push_back(tasks[tsize - i]);
+        currentVec.push_back(uMap[tasks[i]].back());
+        uMap[tasks[i]].pop_back();
+        currentVec.push_back(uMap[tasks[tsize - i]].back());
+        uMap[tasks[tsize - i]].pop_back();
         retVal.push_back(currentVec);
     }
     return retVal;
