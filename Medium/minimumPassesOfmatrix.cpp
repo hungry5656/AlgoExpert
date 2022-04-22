599e6ab3366018e32c243f526168f4c2cd4dde1f
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-22 00:18:09 -0700

    init 3 files

---
+++ Medium/minimumPassesOfmatrix.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+using namespace std;
+
+int minimumPassesOfMatrix(vector<vector<int>> matrix) {
+    
+    return -1;
+}
---
+++ Medium/removeIslands.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+using namespace std;
+
+vector<vector<int>> removeIslands(vector<vector<int>> matrix) {
+    
+    return matrix;
+}
