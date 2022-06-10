5c9c19b30f402206946767e752a76859881d2f0a
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-09 21:49:33 -0700

    finished kadane's algorithm

--- Medium/KadanesAlgorithm.cpp
+++ Medium/KadanesAlgorithm.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int kadanesAlgorithm(vector<int> array) {
-    
-    return -1;
+    // DP
+    vector<int> opt;
+    int currMax;
+    for (int i = 0; i < array.size(); ++i){
+        int numInsert;
+        if (i == 0){
+            numInsert = array[i];
+            currMax = numInsert;
+        } else {
+            numInsert = array[i] + opt[i - 1];
+            if (array[i] > numInsert){
+                numInsert = array[i];
+            }
+        }
+        if (numInsert > currMax){
+            currMax = numInsert;
+        }
+        opt.push_back(numInsert);
+    }
+    return currMax;
 }
