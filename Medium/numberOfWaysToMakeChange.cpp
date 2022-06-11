7851e2e50e1fabe6d354d383255e5cb3b9cfd38a
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-10 22:07:31 -0700

    finished dp problem 2

--- Medium/numberOfWaysToMakeChange.cpp
+++ Medium/numberOfWaysToMakeChange.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int numberOfWaysToMakeChange(int n, vector<int> denoms) {
-    
-    return -1;
+    // Bellman Equation: opt[i] += opt[i - denom];
+    vector<int> opt(n + 1, 0);
+    opt[0] = 1;
+    for (int i = 0; i < denoms.size(); ++i){
+        int numDenom = denoms[i];
+        for (int j = 1; j < opt.size(); ++j){
+            if ((j - numDenom) >= 0){
+                opt[j] += opt[j - numDenom];
+            }
+        }
+    }
+    return opt.back();
 }
