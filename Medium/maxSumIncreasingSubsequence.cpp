a11464f188b5f09d31a9788337b12cefc9d1007a
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-12 21:32:38 -0700

    update

---
+++ Medium/maxSumIncreasingSubsequence.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> maxSumIncreasingSubsequence(vector<int> array) {
+    vector<int> sortList = array;
+    sort(sortList.begin(), sortList.end());
+    map<int, int> findPrev;
+    for (int i = 0; i < array.size(); ++i){
+        if ()
+    }
+    return {
+        {0}, // Sum of sequence.
+        {},  // Elements of the sequence.
+    };
+}
