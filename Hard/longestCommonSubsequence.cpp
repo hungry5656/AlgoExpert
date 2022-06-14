090cb9ba3e9855b5b92e5ccaf35bcd1545bf810f
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-13 23:44:40 -0700

    finished basic step for dp 7

--- Hard/longestCommonSubsequence.cpp
+++ Hard/longestCommonSubsequence.cpp
@@ -1,8 +1,32 @@
 #include <vector>
 #include <string>
+#include <iostream>
 using namespace std;
 
 vector<char> longestCommonSubsequence(string str1, string str2) {
     // Bellman Equation: 
-    return {};
+    // opt[i][j] = {opt[i - 1][j - 1] + 1, if str1[i] == str2[j];
+    //              MAX(opt[i - 1][j], opt[i][j - 1]), else}
+    vector<vector<int>> opt;
+    for (int i = 0; i <= str1.length(); ++i){
+        opt.push_back({});
+        // cout << "[";
+        for (int j = 0; j <= str2.length(); ++j){
+            if (i == 0){
+                opt[i].push_back(0);
+            } else if (j == 0){
+                opt[i].push_back(0);
+            } else {
+                
+                if (str1[i - 1] == str2[j - 1]){
+                    opt[i].push_back(opt[i - 1][j - 1] + 1);
+                } else {
+                    opt[i].push_back(max(opt[i - 1][j], opt[i][j - 1]));
+                }
+            }
+            // cout << opt[i][j] << ", ";
+        }
+        // cout << "]" << endl;
+    }
+    return {to_string(opt[str1.length()][str2.length()])[0]};
 }
