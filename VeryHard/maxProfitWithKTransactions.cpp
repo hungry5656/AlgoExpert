d2bc85bf983e936f3aa877307b71e185ad69567c
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-15 23:41:37 -0700

    finished dp very hard p1

--- VeryHard/maxProfitWithKTransactions.cpp
+++ VeryHard/maxProfitWithKTransactions.cpp
@@ -1,7 +1,40 @@
 #include <vector>
+#include <cmath>
+#include <climits>
+#include <iostream>
 using namespace std;
 
 int maxProfitWithKTransactions(vector<int> prices, int k) {
-    
-    return -1;
+    // similar to a - b + c - b
+    // In this case, it is - a + b - c + d ...
+    // for each (-m + n) pair, record its maximum and compare with other culmulative sigma(-m + n);
+    // Time Complexity: O(nk); Space Complexity: O(n);
+    if (prices.size() < 2){return 0;}
+    if (2*k > prices.size()){
+        k = floor(prices.size()/2);
+    }
+    vector<int> opt[2] = {prices, prices};
+    int retMax = 0;
+    for (int kitr = 0; kitr < 2*k; ++kitr){
+        cout << kitr << endl;
+        bool vecIndex = kitr % 2;
+        int currExtNum = INT_MIN;
+        for (int i = kitr; i < prices.size(); ++i){
+            int currNum;
+            if (kitr == 0){
+                currNum = - prices[i];
+            } else if (!vecIndex){
+                currNum = opt[!vecIndex][i - 1] - prices[i];
+            } else {
+                currNum = opt[!vecIndex][i - 1] + prices[i];
+            }
+            currExtNum = max(currExtNum, currNum);
+            opt[vecIndex][i] = currExtNum;
+            cout << vecIndex << " " << i << ": " << currExtNum << endl;
+        }
+        if (vecIndex){
+            retMax = max(opt[vecIndex].back(), retMax);
+        }
+    }
+    return retMax;
 }
