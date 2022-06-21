4bf5925bdc7e3a8f34cb920561ad33a2ea352bbc
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-20 22:38:25 -0700

    update graph mid 2

--- Medium/cycleInGraph.cpp
+++ Medium/cycleInGraph.cpp
@@ -1,9 +1,30 @@
 #include <vector>
 using namespace std;
 
+bool detectCycle(vector<vector<int>> &edges, vector<bool> pred, int currNode);
+
 bool cycleInGraph(vector<vector<int>> edges) {
-    for (int i = 0; i < edges.size(); ++i){
-        
+    vector<bool> pred(edges.size(), 0);
+    int i = 0;
+    while (i < edges.size()){
+        if (edges[i].size() != 0){
+            break;
+        }
+        ++i;
+    }
+    if (i == edges.size()){return false;}
+    return detectCycle(edges, pred, i);
+}
+
+bool detectCycle(vector<vector<int>> &edges, vector<bool> pred, int currNode){
+    if (pred[currNode]){
+        return true;
+    }
+    pred[currNode] = 1;
+    for (int i = 0; i < edges[currNode].size(); ++i){
+        if(detectCycle(edges, pred, edges[currNode][i])){
+            return true;
+        }
     }
     return false;
 }
