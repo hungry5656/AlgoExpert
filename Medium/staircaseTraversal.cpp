336d7497355bb6e6649ff70e7f55fbafd2828a60
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-23 00:11:34 -0700

    finished recursive prob 6 by a non-efficient algorithm

--- Medium/staircaseTraversal.cpp
+++ Medium/staircaseTraversal.cpp
@@ -1,6 +1,16 @@
 using namespace std;
 
 int staircaseTraversal(int height, int maxSteps) {
+    // recursion: O(k^n)
+    if (height == 0){
+        return 1;
+    }
+    int currRet = 0;
+    for (int i = 1; i <= maxSteps; ++i){
+        if (height - i >= 0){
+            currRet += staircaseTraversal(height - i, maxSteps);
+        }
+    }
     
-    return -1;
+    return currRet;
 }
