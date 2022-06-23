2d3f2680c01884274ca41e05a30e71836cf93274
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-23 00:36:38 -0700

    better time and space complexity implemented

--- Medium/staircaseTraversal.cpp
+++ Medium/staircaseTraversal.cpp
@@ -1,7 +1,8 @@
+#include <vector>
 using namespace std;
-
+/*
 int staircaseTraversal(int height, int maxSteps) {
-    // recursion: O(k^n)
+    // recursion: time Complexity: O(k^n); Space Complexity: O(k^n)
     if (height == 0){
         return 1;
     }
@@ -13,4 +14,23 @@ int staircaseTraversal(int height, int maxSteps) {
     }
     
     return currRet;
+}
+*/
+
+int staircaseTraversal(int height, int maxSteps) {
+    // Sliding window Approach:  time Complexity: O(n); Space Complexity: O(n)
+    vector<int> retVec(height + 1);
+    retVec[0] = 1;
+    retVec[1] = 1;
+    int currWindow = 1;
+    for (int i = 2; i <= height; ++i){
+        if (currWindow < maxSteps){
+            retVec[i] = retVec[i - 1] * 2;
+            ++currWindow;
+        } else {
+            retVec[i] = retVec[i - 1] * 2 - retVec[i - 1 - maxSteps];
+        }
+    }
+    
+    return retVec[height];
 }
