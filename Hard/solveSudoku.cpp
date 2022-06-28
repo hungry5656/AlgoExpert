22929a88a26965b0cce15220fa7d9df183ce7637
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-27 23:49:24 -0700

    update

--- Hard/solveSudoku.cpp
+++ Hard/solveSudoku.cpp
@@ -1,7 +1,19 @@
 #include <vector>
 using namespace std;
 
+vector<vector<int>> recursiveSolve(vector<vector<int>> board, int i, int j);
+
 vector<vector<int>> solveSudoku(vector<vector<int>> board) {
-    
+    for (int i = 0; i < board.size(); ++i){
+        for (int j = 0; j < board[i].size(); ++j){
+            if (board[i][j] == 0){
+                board = recursiveSolve(board, i, j);
+            }
+        }
+    }
     return {};
+}
+
+vector<vector<int>> recursiveSolve(vector<vector<int>> board, int i, int j){
+
 }
