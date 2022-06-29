060019162e3cdbdae3a4d923d9ea638132793e53
hungry5656 <hhhhhenrysun@gmail.com>
2022-06-28 23:53:52 -0700

    update code with fixed to void

--- Hard/solveSudoku.cpp
+++ Hard/solveSudoku.cpp
@@ -1,19 +1,14 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> recursiveSolve(vector<vector<int>> board, int i, int j);
+void recursiveSolve(vector<vector<int>> &board, int i, int j);
 
 vector<vector<int>> solveSudoku(vector<vector<int>> board) {
-    for (int i = 0; i < board.size(); ++i){
-        for (int j = 0; j < board[i].size(); ++j){
-            if (board[i][j] == 0){
-                board = recursiveSolve(board, i, j);
-            }
-        }
-    }
+    recursiveSolve(board, 0, 0);
     return {};
 }
 
-vector<vector<int>> recursiveSolve(vector<vector<int>> board, int i, int j){
+void recursiveSolve(vector<vector<int>> &board, int i, int j){
+    // backtracking
 
 }
