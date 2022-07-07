09bc373fa79a2d05d0cee5c0029e246dc1db7d14
hungry5656 <hhhhhenrysun@gmail.com>
2022-07-06 22:07:41 -0700

    finish binary search

--- Easy/binarySearch.cpp
+++ Easy/binarySearch.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int binarySearch(vector<int> array, int target) {
-    int i = 0, j = array.size() - 1;
-    
+    int i = 0, j = array.size() - 1, mid;
+    while (i <= j) {
+        mid = (i + j)/2;
+        if (target == array[mid]){
+            return mid;
+        } else if (target > array[mid]){
+            i = mid + 1;
+        } else {
+            j = mid - 1;
+        }
+    }
     return -1;
 }
