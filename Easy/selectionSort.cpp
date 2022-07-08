1569e7acb024efedb24e9c39b6cb41d20e7325e3
hungry5656 <hhhhhenrysun@gmail.com>
2022-07-08 01:15:59 -0700

    finished selection sort

--- Easy/selectionSort.cpp
+++ Easy/selectionSort.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 vector<int> selectionSort(vector<int> array) {
-    
-    return {};
+    for (int i = 0; i < array.size(); ++i){
+        int currIdx = i;
+        for (int j = i + 1; j < array.size(); ++j){
+            if (array[j] < array[currIdx]){
+                currIdx = j;
+            }
+        }
+        swap(array[i], array[currIdx]);
+    }
+    return array;
 }
