10b01ea4a2444b22f66088f5f6964d47df470d9c
hungry5656 <hhhhhenrysun@gmail.com>
2022-07-17 22:49:42 -0700

    finished BinaryTreeDiameter.cpp

--- Medium/binaryTreeDiameter.cpp
+++ Medium/binaryTreeDiameter.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 using namespace std;
 
 // This is an input class. Do not edit.
@@ -14,15 +15,29 @@ public:
     }
 };
 
-int binaryTreeDiameter(BinaryTree *tree, int depth = 0) {
+int maxlength(BinaryTree *tree, int* retVal);
+
+int binaryTreeDiameter(BinaryTree *tree) {
+    int retVal = 0;
+    maxlength(tree, &retVal);
+    return retVal;
+}
+
+int maxlength(BinaryTree *tree, int* retVal){
+    int leftN = 0, rightN = 0;
     if (tree -> left == nullptr && tree -> right == nullptr){
         return 0;
     }
     if (tree -> left != nullptr){
-        
+        leftN = maxlength(tree -> left, retVal) + 1;
     }
-
-    return -1;
+    if (tree -> right != nullptr){
+        rightN = maxlength(tree -> right, retVal) + 1;
+    }
+    int currBig = (leftN > rightN) ? leftN : rightN;
+    int curMax = leftN + rightN;
+    if (curMax > *retVal){
+        *retVal = curMax;
+    }
+    return currBig;
 }
-
-int maxlength()
