40442ea537fb8ec2dc35a0a6ce294a9bfe2c057b
hungry5656 <hhhhhenrysun@gmail.com>
2022-07-21 13:18:39 -0700

    update code

---
+++ Medium/findSuccessor.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+public:
+    int value;
+    BinaryTree *left = nullptr;
+    BinaryTree *right = nullptr;
+    BinaryTree *parent = nullptr;
+
+    BinaryTree(int value) { this->value = value; }
+};
+
+BinaryTree *findSuccessor(BinaryTree *tree, BinaryTree *node) {
+    BinaryTree *cNode = tree;
+    if (node -> right != nullptr){
+
+    }
+    return nullptr;
+}
---
+++ Medium/heightBalancedBinaryTree.cpp
@@ -0,0 +1,17 @@
+
+using namespace std;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+public:
+    int value;
+    BinaryTree *left = nullptr;
+    BinaryTree *right = nullptr;
+
+    BinaryTree(int value) { this->value = value; }
+};
+
+bool heightBalancedBinaryTree(BinaryTree *tree) {
+    
+    return false;
+}
