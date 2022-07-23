77eb6715b21726e1e286880dcff63c2b89c716e5
hungry5656 <hhhhhenrysun@gmail.com>
2022-07-22 22:07:03 -0700

    finished check balance

--- Medium/heightBalancedBinaryTree.cpp
+++ Medium/heightBalancedBinaryTree.cpp
@@ -1,5 +1,6 @@
-
+#include <vector>
 using namespace std;
+
 
 // This is an input class. Do not edit.
 class BinaryTree {
@@ -11,12 +12,21 @@ public:
     BinaryTree(int value) { this->value = value; }
 };
 
+vector<int> getVal(BinaryTree *currNode);
+
 bool heightBalancedBinaryTree(BinaryTree *tree) {
-    
-    if (/* condition */)
-    {
-        /* code */
+    return getVal(tree)[1];
+}
+
+vector<int> getVal(BinaryTree *currNode){
+    if (currNode == nullptr){
+        return {-1, 1};
     }
+
+    vector<int> leftRes = getVal(currNode -> left);
     
-    return false;
+    vector<int> rightRes = getVal(currNode -> right);
+    bool isBalanced = leftRes[1] && rightRes[1] && (abs(leftRes[0] - rightRes[0]) <= 1);
+    int height = max(leftRes[0], rightRes[0]) + 1;
+    return {height, isBalanced};
 }
