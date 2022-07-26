086e8da2a949dc99adac50933e5d6e629017a604
hungry5656 <hhhhhenrysun@gmail.com>
2022-07-25 23:47:50 -0700

    add code

--- Medium/removeIslands.cpp
+++ Medium/removeIslands.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 vector<vector<int>> removeIslands(vector<vector<int>> matrix) {
+    vector<vector<int>> checklist(matrix.size(), vector<int>(matrix[0].size(), 0));
     
+    for (int i = 0; i < matrix.size(); ++i){
+        for (int j = 0; j < matrix[0].size(); ++j){
+            checklist[i][j] = 1;
+            
+        }
+    }
     return matrix;
 }
