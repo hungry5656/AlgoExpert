d5b25b6cc55789d3108401dfcf930612d5f30172
hungry5656 <hhhhhenrysun@gmail.com>
2022-07-31 23:39:32 -0700

    update removeland

--- Medium/removeIslands.cpp
+++ Medium/removeIslands.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 vector<vector<int>> removeIslands(vector<vector<int>> matrix) {
-    vector<vector<int>> checklist(matrix.size(), vector<int>(matrix[0].size(), 0));
+    vector<vector<int>> checklist(matrix.size(), vector<int>(matrix[0].size(), -2));
+    vector<vector<int>> landlist;
     
     for (int i = 0; i < matrix.size(); ++i){
         for (int j = 0; j < matrix[0].size(); ++j){
-            checklist[i][j] = 1;
-            
+            checklist[i][j] = -1;
+            if (matrix[i][j] == 1){
+                // check around;
+            }
         }
     }
     return matrix;
