fea8eef56ad3552694c5641639688bd7275280f6
hungry5656 <hhhhhenrysun@gmail.com>
2022-08-01 23:19:39 -0700

    add more code

--- Medium/removeIslands.cpp
+++ Medium/removeIslands.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 using namespace std;
+int checkNeighbor(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &checklist);
 
 vector<vector<int>> removeIslands(vector<vector<int>> matrix) {
     vector<vector<int>> checklist(matrix.size(), vector<int>(matrix[0].size(), -2));
@@ -7,11 +8,20 @@ vector<vector<int>> removeIslands(vector<vector<int>> matrix) {
     
     for (int i = 0; i < matrix.size(); ++i){
         for (int j = 0; j < matrix[0].size(); ++j){
-            checklist[i][j] = -1;
             if (matrix[i][j] == 1){
+                checklist[i][j] = checkNeighbor(i, j, matrix, checklist);
                 // check around;
+            } else {
+                checklist[i][j] = -1;
             }
         }
     }
     return matrix;
+}
+
+int checkNeighbor(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &checklist){
+    int rowN = matrix.size(), colN = matrix[0].size();
+    if (i - 1 >= 0){
+        
+    }
 }
