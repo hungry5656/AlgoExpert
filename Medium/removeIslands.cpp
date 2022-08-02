a7f0a1ce53fedb2b94086f35dfa75c210faea0f0
hungry5656 <hhhhhenrysun@gmail.com>
2022-08-02 15:53:24 -0700

    fix some code

--- Medium/removeIslands.cpp
+++ Medium/removeIslands.cpp
@@ -1,27 +1,48 @@
 #include <vector>
 using namespace std;
-int checkNeighbor(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &checklist);
+void checkNeighbor(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &checklist);
 
 vector<vector<int>> removeIslands(vector<vector<int>> matrix) {
     vector<vector<int>> checklist(matrix.size(), vector<int>(matrix[0].size(), -2));
-    vector<vector<int>> landlist;
+    vector<vector<pair<int, int>>> landlist;
     
     for (int i = 0; i < matrix.size(); ++i){
         for (int j = 0; j < matrix[0].size(); ++j){
+            if (checklist[i][j] != -2){
+                continue;
+            }
             if (matrix[i][j] == 1){
-                checklist[i][j] = checkNeighbor(i, j, matrix, checklist);
+                checklist[i][j] = landlist.size();
+                checkNeighbor(i, j, matrix, checklist);
                 // check around;
             } else {
                 checklist[i][j] = -1;
             }
         }
     }
+    // check land
+
+    // delete land
+
     return matrix;
 }
 
-int checkNeighbor(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &checklist){
+void checkNeighbor(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &checklist){
     int rowN = matrix.size(), colN = matrix[0].size();
     if (i - 1 >= 0){
-        if (checklist[i - 1][j] != )
+        if (checklist[i - 1][j] != -2 ){
+            if (matrix[i - 1][j] == 1){
+            
+            }
+        }
+    }
+    if (i + 1 < rowN){
+
+    }
+    if (j - 1 >= 0){
+
+    }
+    if (j + 1 < colN){
+        
     }
 }
