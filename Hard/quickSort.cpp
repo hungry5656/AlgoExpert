9df285041f9f7789327d1c40d3ccc5940788f9f7
hungry5656 <hhhhhenrysun@gmail.com>
2022-08-02 17:12:38 -0700

    add some basic structure to quicksort

--- Hard/quickSort.cpp
+++ Hard/quickSort.cpp
@@ -1,7 +1,12 @@
 #include <vector>
 using namespace std;
+void qSort(vector<int> &array, int start, int end);
 
 vector<int> quickSort(vector<int> array) {
+    qSort(array, 0, array.size() - 1);
+    return array;
+}
+
+void qSort(vector<int> &array, int start, int end){
     
-    return {};
 }
