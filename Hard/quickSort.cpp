5b6b630dbd15aae1b892d959765820491d3f7684
hungry5656 <hhhhhenrysun@gmail.com>
2022-08-02 17:06:32 -0700

    initialize quicksort.cpp

---
+++ Hard/quickSort.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+using namespace std;
+
+vector<int> quickSort(vector<int> array) {
+    
+    return {};
+}
--- Hard/solveSudoku.cpp
+++ Hard/solveSudoku.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void recursiveSolve(vector<vector<int>> &board, int i, int j);
 
 vector<vector<int>> solveSudoku(vector<vector<int>> board) {
+    pair<int, int> blank;
     recursiveSolve(board, 0, 0);
     return {};
 }
