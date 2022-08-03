d2149bb6616a6c553e8768afb278b46e0fc4f300
hungry5656 <hhhhhenrysun@gmail.com>
2022-08-02 23:42:14 -0700

    finished quicksort

--- Hard/quickSort.cpp
+++ Hard/quickSort.cpp
@@ -8,5 +8,25 @@ vector<int> quickSort(vector<int> array) {
 }
 
 void qSort(vector<int> &array, int start, int end){
-    
+    if (start >= end){
+        return;
+    }
+    int Pivot = start, lNode = start + 1, rNode = end;
+    while (lNode <= rNode){
+        if (array[lNode] > array[Pivot] && array[rNode] < array[Pivot]){
+            swap(array[lNode], array[rNode]);
+            lNode++;
+            rNode--;
+        } else if (array[lNode] > array[Pivot]){
+            rNode--;
+        } else if (array[rNode] < array[Pivot]){
+            lNode++;
+        } else {
+            lNode++;
+            rNode--;
+        }
+    }
+    swap(array[Pivot], array[rNode]);
+    qSort(array, start, rNode - 1);
+    qSort(array, rNode + 1, end);
 }
