8aa80a588c41c5430f94d45f4c763994486572d0
hungry5656 <hhhhhenrysun@gmail.com>
2022-12-17 12:57:19 -0800

    add check function

--- Hard/solveSudoku.cpp
+++ Hard/solveSudoku.cpp
@@ -1,18 +1,54 @@
 #include <vector>
 using namespace std;
 
-void recursiveSolve(vector<vector<int>> &board, int i, int j);
+bool recursiveSolve(vector<vector<int>> &board, int i, int j);
+bool checkValidTry(vector<vector<int>> &board, int i, int j, int posVal);
 
 vector<vector<int>> solveSudoku(vector<vector<int>> board) {
     pair<int, int> blank;
     recursiveSolve(board, 0, 0);
-    return {};
+    return board;
 }
 
-void recursiveSolve(vector<vector<int>> &board, int i, int j){
+bool recursiveSolve(vector<vector<int>> &board, int i, int j){
     // backtracking
     if (i == 9 and j == 9)
         return;
     
     
+    return recursiveSolve(board, i, j + 1);
+}
+
+bool checkValidTry(vector<vector<int>> &board, int i, int j, int posVal){
+    // check row and col
+    for (int itr = 0; itr < 9; ++itr){
+        if (board[i][itr] == posVal)
+            return false;
+        if (board[itr][j] == posVal)
+            return false;
+    }
+    // check diagnal
+    if (i == j){
+        for (int itr = 0; itr < 9; ++itr){
+            if(board[itr][itr] == posVal)
+                return false;
+        }
+    }
+    if (i + j == 8){
+        for (int itr = 0; itr < 9; ++itr){
+            if(board[itr][8 - itr] == posVal)
+                return false;
+        }
+    }
+    // check subsquare
+    int bigX, bigY;
+    bigX = i / 3;
+    bigY = j / 3;
+    for (int bx = 3 * bigX; bx < 3 * bigX + 3; ++bx){
+        for (int by = 3 * bigY; by < 3 * bigY + 3; ++by){
+            if (board[bx][by] == posVal)
+                return false;
+        }
+    }
+    return true;
 }
