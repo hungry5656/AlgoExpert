5b58e3547fa22b612f738d03c15f3d3d12f1bae6
hungry5656 <hhhhhenrysun@gmail.com>
2022-12-17 18:36:33 -0800

    finish iterative loop without diagonal check

--- Hard/solveSudoku.cpp
+++ Hard/solveSudoku.cpp
@@ -1,20 +1,39 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 bool recursiveSolve(vector<vector<int>> &board, int i, int j);
 bool checkValidTry(vector<vector<int>> &board, int i, int j, int posVal);
 
 vector<vector<int>> solveSudoku(vector<vector<int>> board) {
-    pair<int, int> blank;
     recursiveSolve(board, 0, 0);
     return board;
 }
 
 bool recursiveSolve(vector<vector<int>> &board, int i, int j){
     // backtracking
-    if (i == 9 and j == 9)
-        return;
+    if (i == 8 and j == 9)
+        return true;
     
+    if (j == 9){
+        i++;
+        j = 0;
+    }
+    cout << "i, j: " << i << " " << j << endl;
+
+    // if it is blank, try to insert a valid val
+    if (board[i][j] == 0){
+        for (int numVal = 1; numVal < 10; ++numVal){
+            if (checkValidTry(board, i, j, numVal)){
+                board[i][j] = numVal;
+                // cout << "Hello\n";
+                if (recursiveSolve(board, i, j + 1))
+                    return true;
+            }
+        }
+        board[i][j] = 0;
+        return false;
+    }
     
     return recursiveSolve(board, i, j + 1);
 }
@@ -28,18 +47,18 @@ bool checkValidTry(vector<vector<int>> &board, int i, int j, int posVal){
             return false;
     }
     // check diagnal
-    if (i == j){
-        for (int itr = 0; itr < 9; ++itr){
-            if(board[itr][itr] == posVal)
-                return false;
-        }
-    }
-    if (i + j == 8){
-        for (int itr = 0; itr < 9; ++itr){
-            if(board[itr][8 - itr] == posVal)
-                return false;
-        }
-    }
+    // if (i == j){
+    //     for (int itr = 0; itr < 9; ++itr){
+    //         if(board[itr][itr] == posVal)
+    //             return false;
+    //     }
+    // }
+    // if (i + j == 8){
+    //     for (int itr = 0; itr < 9; ++itr){
+    //         if(board[itr][8 - itr] == posVal)
+    //             return false;
+    //     }
+    // }
     // check subsquare
     int bigX, bigY;
     bigX = i / 3;
