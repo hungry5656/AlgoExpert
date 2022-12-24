1559f80f6d39bec7f6c9ed02d12a2c451edc7d54
hungry5656 <hhhhhenrysun@gmail.com>
2022-12-24 01:19:50 -0800

    finish sortStack and add nextGreaterELement

---
+++ Medium/nextGreaterElement.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int> array) {
+    
+    return {};
+}
--- Medium/sortStack.cpp
+++ Medium/sortStack.cpp
@@ -1,7 +1,30 @@
 #include <vector>
 using namespace std;
 
-vector<int> sortStack(vector<int> &stack) {
+void sortStackwithVal(vector<int> &stack, int val){
+    if (stack.size() == 0 || stack.back() <= val){
+        stack.push_back(val);
+        return;
+    }
+    int LastNum = stack.back();
+    stack.pop_back();
     
-    return {};
+    sortStackwithVal(stack, val);
+
+    stack.push_back(LastNum);
+}
+
+vector<int> sortStack(vector<int> &stack) {
+    if (stack.size() == 0){
+        return stack;
+    }
+
+    int LastNum = stack.back();
+    stack.pop_back();
+
+    sortStack(stack);
+
+    sortStackwithVal(stack, LastNum);
+
+    return stack;
 }
