468b45b9d0ed9d8dadb59bba8cf8d4429840e63a
hungry5656 <hhhhhenrysun@gmail.com>
2023-10-13 12:05:27 -0700

    finished generateDivTags

--- Hard/generateDivTags.cpp
+++ Hard/generateDivTags.cpp
@@ -2,7 +2,25 @@
 #include <string>
 using namespace std;
 
+void recursiveGen(int numberOfTags, int headUsed, int tailUsed, vector<string> &retVec, string currStr);
+
 vector<string> generateDivTags(int numberOfTags) {
-    
-    return ;
+    vector<string> retVec;
+    recursiveGen(numberOfTags, 0, 0, retVec, "");
+    return retVec;
+}
+void recursiveGen(int numberOfTags, int headUsed, int tailUsed, vector<string> &retVec, string currStr){
+    if (tailUsed == numberOfTags){
+        retVec.push_back(currStr);
+        return;
+    }
+    if (headUsed < numberOfTags){
+        recursiveGen(numberOfTags, headUsed + 1, tailUsed, retVec, currStr + "<div>");
+    }
+    if (tailUsed < numberOfTags){
+        if (tailUsed + 1 <= headUsed){
+            recursiveGen(numberOfTags, headUsed, tailUsed + 1, retVec, currStr + "</div>");
+        }
+    }
+    return;
 }
